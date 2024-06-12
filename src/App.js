c0e02c035b0a200f2603a12553f4104dd21c5b26
izzylppz <https://github.com/Shmizzy>
2024-06-12 16:17:36 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [todos, setTodos] = useState([])
+  const [formData, setFormData] = useState({
+    name: ''
+  })
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTodos([...todos, formData.name]);
+    setFormData({
+      name: ''
+    })
+  }
+
+  return <>
+    <h1>Todo App</h1>
+    <div className="todos" data-testid="todos">{todos.map((todo, index) => (
+      <p key={index}>{todo}</p>
+    ))}</div>
+    <label htmlFor="name">Todo Name: </label>
+    <input id="name" name="name" placeholder="TODO" onChange={handleChange} value={formData.name}></input>
+    <button onClick={handleSubmit}>Add Todo</button>
+  </>
 }
+
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,38 @@
-import { render, screen } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders an h1 tag', ()=> {
+  const app = render(<App />);
+  const heading = app.getByRole('heading');
+  expect(heading).toHaveTextContent('Todo App')
+})
+
+test('The label exists', () => {
+  const app = render(<App />);
+  const label = app.getByText('Todo Name:');
+  expect(label).toBeInTheDocument()
+})
+
+test('The input exists', () => {
+  const app = render(<App />);
+  const input = app.getByPlaceholderText('TODO')
+  expect(input).toBeInTheDocument();
+})
+
+test('The button exists', () => {
+  const app = render(<App />);
+  const button = app.getByText('Add Todo')
+  expect(button).toBeInTheDocument();
+})
+
+test('The button event works and is adding element to todo div', () => {
+  const app = render(<App />);
+  const input = app.getByPlaceholderText('TODO');
+  const button = app.getByText('Add Todo');
+
+  fireEvent.change(input, {target: {value: 'Test Todo'}});
+  fireEvent.click(button);
+
+  const todosDiv = app.getByTestId('todos');
+  expect(todosDiv).toHaveTextContent('Test Todo')
+})
